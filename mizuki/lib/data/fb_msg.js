fff7dbc9131fa522727a515008186ed43d57f401
Black Kingdom <94972950+BlackKingdoms@users.noreply.github.com>
2021-12-02 12:50:52 +0530

    Update fb_msg.js

--- mizuki/lib/data/fb_msg.js
+++ mizuki/lib/data/fb_msg.js
@@ -3,8 +3,8 @@ const os = require("os");
 const axios = require('axios');
 
 async function fb_msg(Language) {
-    var sdn = 'RUN git clone https://github.com/MrJoka-Thejaka/Mizuki /root/QueenMizuki' + '\n'
-    exec('sed -n 3p /root/QueenMizuki/Mizuki/Dockerfile', async (err, stdout, stderr) => {
+    var sdn = 'RUN git clone https://github.com/BlackKingdoms/BlackKingdom /root/BlackKingdom' + '\n'
+    exec('sed -n 3p /root/BlackKingdom/Dockerfile', async (err, stdout, stderr) => {
       if (sdn !== stdout) {
         throw new Error("Fake - Unknown Device !!");
       }
@@ -18,16 +18,7 @@ async function fb_msg(Language) {
     username: '',
     comment: '',
     creation: ''
-  }
-  if (Language == 'SI') {
-    data.title = '*Title:* '
-    data.caption = '*Caption:* '
-    data.like = '*Like Count:* '
-    data.play = '*Views:* '
-    data.share = '*Share Count:* '
-    data.username = '*Username:* '
-    data.comment = '*Comment Count:* '
-    data.creation = '*Creation Date:* '
+
   }
   else if (Language == 'EN') {
     data.title = '*Title:* '
